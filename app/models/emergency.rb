07293b1ba334573404fb7e74939194e5f826d6d2
elliotthilaire <elliott.hilaire@gmail.com>
2015-04-24 09:13:46 +1000

    tidied models

--- app/models/emergency.rb
+++ app/models/emergency.rb
@@ -1,12 +1,5 @@
 class Emergency < ActiveRecord::Base
-
-  validates_uniqueness_of :code
-  validates_presence_of :code, :fire_severity, :police_severity, :medical_severity
-  
-  validates_numericality_of :fire_severity, greater_than_or_equal_to: 0
-  validates_numericality_of :police_severity, greater_than_or_equal_to: 0
-  validates_numericality_of :medical_severity, greater_than_or_equal_to: 0
-
+  validates :code,  uniqueness: true
+  validates :code, :fire_severity, :police_severity, :medical_severity, presence: true
+  validates :fire_severity, :police_severity, :medical_severity, numericality: true, minimum: 0
 end
-
-
--- app/models/responder.rb
+++ app/models/responder.rb
@@ -1,12 +1,8 @@
 class Responder < ActiveRecord::Base
-
   # 'type' is reserved as a column name
   alias_attribute :type, :type_of
 
-  validates_uniqueness_of :name
-  validates_presence_of :name, :type, :capacity
-  validates :capacity, :inclusion => 1..5
-
-
-
+  validates :name,  uniqueness: true
+  validates :name, :type, :capacity, presence: true
+  validates :capacity, inclusion: 1..5
 end
