c03eead790447336bc10c4af7c2b051fdfa5c7ed
elliotthilaire <elliott.hilaire@gmail.com>
2015-04-24 09:46:38 +1000

    fixed regression, validation not formatted properly

--- app/models/emergency.rb
+++ app/models/emergency.rb
@@ -1,5 +1,5 @@
 class Emergency < ActiveRecord::Base
   validates :code,  uniqueness: true
   validates :code, :fire_severity, :police_severity, :medical_severity, presence: true
-  validates :fire_severity, :police_severity, :medical_severity, numericality: true, minimum: 0
+  validates :fire_severity, :police_severity, :medical_severity, numericality: { greater_than: 0 }
 end
