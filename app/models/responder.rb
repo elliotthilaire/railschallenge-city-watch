734756ed25e452648793d323db6d9cd3db8f7fd0
elliotthilaire <elliott.hilaire@gmail.com>
2015-04-24 13:26:25 +1000

    added scopes to model

--- app/models/responder.rb
+++ app/models/responder.rb
@@ -2,6 +2,11 @@ class Responder < ActiveRecord::Base
   # 'type' is reserved as a column name
   alias_attribute :type, :type_of
 
+  scope :by_type, -> (type) { where(type: type) }
+  scope :available, -> { where(emergency_code: nil) }
+  scope :on_duty, -> { where(on_duty: true)}
+  scope :ready_for_dispatch, -> { available.on_duty }
+
   validates :name,  uniqueness: true
   validates :name, :type, :capacity, presence: true
   validates :capacity, inclusion: 1..5
