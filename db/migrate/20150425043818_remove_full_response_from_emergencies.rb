a67e888024e81f4f7bf5f33a45161caf022c296d
elliotthilaire <elliott.hilaire@gmail.com>
2015-04-25 15:04:32 +1000

    need full_response, can't be calculate. oops

--- db/migrate/20150425043818_remove_full_response_from_emergencies.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFullResponseFromEmergencies < ActiveRecord::Migration
-  def change
-    remove_column :emergencies, :full_response, :boolean
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425043818) do
+ActiveRecord::Schema.define(version: 20150425043514) do
 
   create_table "emergencies", force: :cascade do |t|
     t.string   "code"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150425043818) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.datetime "resolved_at"
+    t.boolean  "full_response"
   end
 
   create_table "responders", force: :cascade do |t|
