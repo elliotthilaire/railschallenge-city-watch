3455f68367497590c9d43b9df227932f7f91cc6d
elliotthilaire <elliott.hilaire@gmail.com>
2015-04-25 20:03:33 +1000

    refactor

--- app/services/capacity_report.rb
+++ app/services/capacity_report.rb
@@ -2,18 +2,22 @@ class CapacityReport
   def self.generate
     capacity_report = {}
     # get the unique types
-    types = Responder.uniq.pluck(:type)
+    types = %w(Fire Medical Police)
     # generate as a hash of arrays
     types.each do |type|
-      capacity_report[type] =
-        [
-          Responder.by_type(type).sum(:capacity),
-          Responder.by_type(type).available.sum(:capacity),
-          Responder.by_type(type).on_duty.sum(:capacity),
-          Responder.by_type(type).ready_for_dispatch.sum(:capacity)
-        ]
+      capacity_report[type] = generate_report_for_type(type)
     end
     # return the finished report
     capacity_report
+  end
+
+  def generate_report_for_type(type)
+    responders = Responder.by_type(type)
+    [
+      responders.sum(:capacity),
+      responders.available.sum(:capacity),
+      responders.by_type(type).on_duty.sum(:capacity),
+      responders.by_type(type).ready_for_dispatch.sum(:capacity)
+    ]
   end
 end
